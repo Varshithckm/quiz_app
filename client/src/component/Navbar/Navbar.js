96dc079cbc4ec38fabde578a7df29fac922a4504
Varshith R <varshithvarshi8613@gmail.com>
2024-01-01 16:17:16 +0530

    quizapp

--- client/src/component/Navbar/Navbar.js
+++ client/src/component/Navbar/Navbar.js
@@ -1,13 +1,13 @@
 import React from 'react'
 
-import {Ritlogo} from '../../images/index'
+import {Ritlogo,Govtlogo, Kscstlogo} from '../../images/index'
 
 const Navbar = () => {
   return (
-    <div className='h-12 z-1 bg-white'>
+    <div className='h-15 z-1 bg-transparent'>
       <nav className="flex items-center justify-between flex-wrap px-4 py-3 w-full">
-        <img src="" alt="" />
-        <img src="" alt="" />
+        <img src={Govtlogo}   className='w-20 h-7' />
+        <img src={Kscstlogo}  className='w-20 h-7' />
         <img src={Ritlogo} className='w-20 h-7' />
             </nav>
     </div>
--- client/src/images/index.js
+++ client/src/images/index.js
@@ -1,5 +1,10 @@
 import Ritlogo from './ritlogo.png'
+import Govtlogo from './govtlogo.png'
+import Kscstlogo from './KSCST_WebLogo.jpg'
 export 
 {
-    Ritlogo
+    Ritlogo,
+    Govtlogo,
+    Kscstlogo
+
 }
